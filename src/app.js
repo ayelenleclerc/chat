0f4a6bbd4e26415dc40bedc1c570e8974e6745cc
ayelenleclerc <ayelenleclerc@gmail.com>
2023-08-12 17:02:46 -0300

    Chat final

--- src/app.js
+++ src/app.js
@@ -2,42 +2,28 @@ import express from "express";
 import { Server } from "socket.io";
 import handlebars from "express-handlebars";
 import __dirname from "./utils.js";
-import viewRouter from "./routes/views.router.js";
+import viewsRouter from "./routes/views.router.js";
 
 const app = express();
-
-const PORT = 8080;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(`${__dirname}/public`));
 
 app.engine("handlebars", handlebars.engine());
-
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "handlebars");
 
-app.use("/", viewRouter);
+app.use("/", viewsRouter);
 
-const server = app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
-
+const server = app.listen(8081, () => console.log("Server running"));
 const io = new Server(server);
 
 const messages = [];
+
 io.on("connection", (socket) => {
-  console.log("Nuevo usuario conectado");
+  console.log("Nuevo cliente conectado");
+
   socket.on("message", (data) => {
     messages.push(data);
     io.emit("messageLogs", messages);
-  });
-  socket.on("Authenticated", (data) => {
-    socket.emit("messageLogs", messages);
-    socket.broadcast.emit("newUserConnected", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Usuario desconectado");
   });
 });
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -2,15 +2,14 @@
 
 const socket = io();
 let user;
-const chatBox = document.getElementById("chatBox");
+const chatbox = document.getElementById("chatBox");
+
 Swal.fire({
-  title: "Registrese",
+  title: "Identifiquesé",
   input: "text",
-  text: "Ingresa tu nombre de usuario",
+  text: "Ingresa tu nombre de usuario para ingresar en el chat",
   inputValidator: (value) => {
-    return (
-      !value && "Por favor ingrese un nombre de usuario para ingresar al chat"
-    );
+    return !value && "Ingresa tu nombre de usuario para ingresar en el chat";
   },
   allowOutsideClick: false,
   allowEscapeKey: false,
@@ -19,11 +18,11 @@ Swal.fire({
   socket.emit("authenticated", user);
 });
 
-chatBox.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    if ((chatBox.value.trim(), length > 0)) {
-      socket.emit("message", { user, message: chatBox.value });
-      chatBox.value = "";
+chatbox.addEventListener("keyup", (evt) => {
+  if (evt.key === "Enter") {
+    if (chatbox.value.trim().length > 0) {
+      socket.emit("message", { user, message: chatbox.value });
+      chatbox.value = "";
     }
   }
 });
@@ -32,18 +31,18 @@ socket.on("messageLogs", (data) => {
   let log = document.getElementById("messageLogs");
   let messages = "";
   data.forEach((message) => {
-    messages += `${message.user} dice: ${message.message}</br>`;
+    messages += `${message.user} dice: ${message.message}<br/>`;
   });
   log.innerHTML = messages;
 });
-
+//cada vez que se conecta un nuevo usuario vamos a mostrar un modal
 socket.on("newUserConnected", (data) => {
   Swal.fire({
     toast: true,
     position: "top-end",
     showConfirmationButton: false,
-    timer: 1500,
-    title: `${data} se ha unido al chat`,
+    timer: 3000,
+    title: `${data} sa ha unido al chat`,
     icon: "success",
   });
 });
